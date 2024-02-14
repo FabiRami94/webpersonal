5238ac3e489f6ea36cefbd2ef334bfd81f013a28
FabiRami94 <fabioerv_1994@hotmail.com>
2024-02-13 21:18:36 -0500

    inclusion fonts home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -28,18 +28,17 @@ const Home = () => {
       }, [toggle]);
 
     return(
-        <>
             <body style={!toggleColor ? { backgroundColor: 'rgb(9, 12, 24)'} : null} className={styles.divGeneral}>
                 <div style={toggleColor ? { color: 'black' } : null} className={styles.divTexto}>
                     <div  className={styles.divTitleChange}>
-                      <h1 className={`${styles.p1} ${!toggle && styles.transition}`}>
+                      <h1 style={{fontFamily: "Protest Riot", fontWeight: '400'}} className={`${styles.p1} ${!toggle && styles.transition}`}>
                         {`Hey, soy Fabián Ramírez`}
                       </h1>
-                      <h1 className={`${styles.p2} ${toggle && styles.transition}`}>
+                      <h1 style={{fontFamily: "Protest Riot", fontWeight: '400'}} className={`${styles.p2} ${toggle && styles.transition}`}>
                         {`Hey, I'm Fabián Ramírez`}
                       </h1>
                     </div>
-                    <h1 style={toggleColor ? { color: 'rgb(14, 18, 41)'} : null}>
+                    <h1 style={toggleColor ? { color: 'rgb(14, 18, 41)',fontFamily: "Protest Riot", fontWeight: '400' } : {fontFamily: "Protest Riot", fontWeight: '400'}}>
                         {/* <span className={isNeon ? styles.spanNeon : styles.spanSinNeon}>UX/UI</span>&nbsp;
                         <span style={{fontSize: '1.6rem'}} className={isNeon ? styles.spanNeon : styles.spanSinNeon}>Web Designer</span>&nbsp; */}
                         <span style={toggleColor ? {textShadow} : null} className={styles.spanBlue}>Full Stack Developer</span>
@@ -49,22 +48,21 @@ const Home = () => {
                         {`Arquitecto de profesión, y aventurándome en el fascinante mundo de la tecnología. 
                           Apasionado por el aprendizaje continuo y el crecimiento constante. "Si solo haces
                           lo que ya sabes hacer, no vas a llegar a ser más de lo que eres hoy" - 
-                          Kung Fu Panda`}
+                          `}<p style={{fontFamily: 'Protest Revolution', display: 'inline'}}> Kung Fu Panda</p>
                       </p>
                       <p className={`${styles.p2} ${toggle && styles.transition}`}>
                         {`Architect by profession, and venturing into the fascinating world of technology.
                           Passionate about continuous learning and constant growth. "If you only do
                           what you already know how to do, you will not become more than what you are today" -
-                          Kung Fu Panda`}
+                          `}<p style={{fontFamily: 'Protest Revolution', display: 'inline'}}> Kung Fu Panda</p>
                       </p>
                     </div>
                 </div>
                 <div className={styles.divImagen}>
                     <img  className={styles.imagen} alt="PorfesionalImage" src="https://i.ibb.co/TLZ5C3L/Rompecabezas.png"/>
                 </div>
-            </body>            
-        </>
-    )
+            </body>                  
+  )
 }
 
 export default Home;
