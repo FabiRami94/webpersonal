9737d434f0a05e0d589ee87b3180a7083bea5349
FabiRami94 <fabioerv_1994@hotmail.com>
2024-04-06 19:05:36 -0500

    loading incorporated

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,53 @@ import Portafolios from './components/Portafolios/Portafolios';
 import Contact from './components/Contact/Contact';
 import Skills from './components/Skills/Skills';
 import axios from 'axios';
+import Loading from './components/Loading/Loading';
+
+import { useEffect, useState } from 'react';
 axios.defaults.baseURL = 'https://webpersonal-production-cbc4.up.railway.app/'
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  },[]);
+
   return (
     <body>
-      <header style={{position: 'sticky', top: '0', zIndex: '3'}}>
-        <NavBar></NavBar>
-      </header>
-      <main>
-        <div id="home">
-          <Home></Home>
-        </div>
-        <div id="acerca">
-          <About></About>
-        </div>
-        <div id="habilidades">
-          <Skills></Skills>
-        </div>        
-        <div id="portafolio">
-          <Portafolios></Portafolios>
-        </div>
-        <div id="contacto">
-          <Contact></Contact>
-        </div>
-      </main>
-      <div>
-        <DownBar></DownBar>
-      </div>
+      { loading ?
+        <Loading></Loading> :
+      (<>
+        <header style={{position: 'sticky', top: '0', zIndex: '3'}}>
+            <NavBar></NavBar>
+          </header>
+          <main>
+            <div id="home">
+              <Home></Home>
+            </div>
+            <div id="acerca">
+              <About></About>
+            </div>
+            <div id="habilidades">
+              <Skills></Skills>
+            </div>        
+            <div id="portafolio">
+              <Portafolios></Portafolios>
+            </div>
+            <div id="contacto">
+              <Contact></Contact>
+            </div>
+          </main>
+          <div>
+            <DownBar></DownBar>
+          </div>
+      </>)
+      }
     </body>
   );
 }
---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,53 @@
+
+import React from 'react';
+import style from './Loading.module.css';
+
+const Loading = () => {
+    return(
+        <>
+            <div className={style.general}>
+                <img
+                    style={{height: '16rem'}} 
+                    src="https://i.ibb.co/ZGF3qS3/Logo-propio-Blanco.png" 
+                    alt='Logo Propio'/>
+                <div className={style.ballContainer}>
+                    <div  
+                        style={{backgroundColor: 'rgb(35, 39, 47)'}}
+                        className={style.ballDiv}>
+                        <img 
+                            style={{marginTop: '0.2rem'}}
+                            src='https://i.ibb.co/1fwZGfV/Logo-React.png/' 
+                            alt='Logo React' />
+                    </div>
+                    <div 
+                        style={{backgroundColor: 'rgb(227, 79, 38)'}}
+                        className={style.ballDiv}>
+                        <img 
+                            style={{height: '70%', marginTop: '0.6rem'}}
+                            src='https://i.ibb.co/znYM4VS/LogoHtml.png' 
+                            alt='Logo HTML' />
+                    </div>
+                    <div  
+                        style={{backgroundColor: 'rgb(2, 119, 189)'}}
+                        className={style.ballDiv}>
+                        <img 
+                            style={{marginTop: '0.6rem'}}
+                            src='https://i.ibb.co/VjtLHs4/LogoCss.png' 
+                            alt='Logo CSS' />
+                    </div>
+                    <div 
+                        style={{backgroundColor: 'rgb(244, 225, 30)'}} 
+                        className={style.ballDiv}>
+                        <img
+                            style={{marginTop: '0.2rem'}} 
+                            src='https://i.ibb.co/2NJxXnR/Logo-Java-Script.png' 
+                            alt='Logo JavaScript' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Loading;
+
